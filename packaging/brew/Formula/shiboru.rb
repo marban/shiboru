2e6cb00aec57132be0d514cd6ffaa94e67d4842b
Thomas Marban <thomas@marban.com>
2026-03-29 08:02:30 +0200

    formula: bump to v1.0.3

--- packaging/brew/Formula/shiboru.rb
+++ packaging/brew/Formula/shiboru.rb
@@ -3,8 +3,8 @@ class Shiboru < Formula
 
   desc "macOS command-line image optimizer for PNG, JPEG, GIF, SVG, and ICO"
   homepage "https://github.com/marban/shiboru"
-  url "https://github.com/marban/shiboru/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "3720c6ad4dd6b6bce59fd9e915070455f1574ff7e4a34b8afa9eaa4b98731762"
+  url "https://github.com/marban/shiboru/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "306cf195fb0ad2296590b81ad5efda8207330342115e273398bbf6155829d209"
   license "MIT"
 
   depends_on "python@3.13"
